481aa991fc5d2054483fbb78023f3e9d67e03466
JuanCarlosNu <101191435+JuanCarlosNu@users.noreply.github.com>
2023-12-06 20:55:05 -0300

    hero slide 2 title

--- customLocalStore.js
+++ customLocalStore.js
@@ -2,7 +2,7 @@ const translations = {
     es: {
         main_tilte: 'Excelencia en Servicios de Ingeniería',
         main_subtitle: 'Trabajando con equipos de profesionales altamente calificados desde nuestra sede en Toulouse, con una vasta experiencia en diferentes rubros de la industria y utilizando las mejores herramientas del mercado, acercamos soluciones eficaces a la más variada gama de necesidades.',
-        frase: "Trabajando con equipos de profesionales altamente calificados desde nuestra sede en Toulouse, con una vasta experiencia en diferentes rubros de la industria y utilizando las mejores herramientas del mercado, acercamos soluciones eficaces a la más variada gama de necesidades.",
+        main_sider_title:"Para la Industria Siderúrgica",
         destacados:"Nuestros proyectos destacados",
         slider1tit:"Rotador de block de motores",
         slider2tit:"Verificación teórica escudo muelle",
@@ -14,7 +14,7 @@ const translations = {
 fr: {
     main_title: 'L’Excellence des Services  D’Ingeniénierie',
      main_subtitle: 'En travaillant avec des équipes de professionnels hautement qualifiés de notre siège à Toulouse, avec une vaste expérience dans différents domaines de l’industrie et en utilisant les meilleurs outils du marché, nous apportons des solutions efficaces aux besoins les plus variés.',
-    frase: "le trabajee fuertemente tode el tiempe musse de chocolate  de la industria y utilizando las mejores herramientas del mercado, acercamos soluciones eficaces a la más variada gama de necesidades.",
+     main_sider_title: "Sidérurgie",
     destacados:"le trabajó un montón",
     slider1tit:"Rotate le blocké de motoré",
     slider2tit:"la verifique  soi",
@@ -26,7 +26,7 @@ fr: {
 en: {
     main_tilte: 'Excellence in Engineering Services',
     main_subtitle: 'Working with teams of highly qualified professionals from our headquarters in Toulouse, with vast experience in different areas of the industry and using the best tools on the market, we bring effective solutions to the most varied range of needs.',
-    frase: "Working with teams of highly qualified professionals from our headquarters in Toulouse, with extensive experience in various industry sectors and using the best tools in the market, we provide effective solutions to a wide range of needs.",
+    main_sider_title: "For Steelmaking",,
     destacados:"Our highlighted projects",
     slider1tit:"Engines rotator",
     slider2tit:"Verification",
