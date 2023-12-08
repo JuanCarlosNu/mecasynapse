4fc81583c1c2ac2d0854fbda788d1a5c42948dd3
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-12-08 18:02:35 -0300

    added console log

--- customLocalStore.js
+++ customLocalStore.js
@@ -101,7 +101,9 @@ en: {
 
 
 function changeLanguage(language) {
+    console.log(language);
     const elements = document.querySelectorAll('[data-translation-id]');
+    console.log(elements);
 
     elements.forEach(element => {
         const translationId = element.getAttribute('data-translation-id');
