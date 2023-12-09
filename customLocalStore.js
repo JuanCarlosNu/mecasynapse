b1f8c67d3abd81f1b68cf769ad89951a191f7b77
JuanCarlosNu <101191435+JuanCarlosNu@users.noreply.github.com>
2023-12-09 13:45:01 -0300

    data language for projects

--- customLocalStore.js
+++ customLocalStore.js
@@ -23,6 +23,21 @@ const translations = {
         main_simu_title:"Simulaciones",
         main_simu_subtitle:"Simulaciones computacionales de modelos físicos de estática, dinámica, transferencia de calor, vibraciones, mecánica de fluidos y fatiga.",
 
+        proy_rotweb_title:"ROTADOR DE BLOCKS DE MOTORES",
+        proy_rotweb_subtitle:"En este proyecto se diseñó un rotador de blocks de motor para tareas de control entre procesos de mecanizado. Incluyó cálculos dinámicos para seleccionar mando, cálculos para dimensionado y selección de sistema neumático para movimiento de marco y clampeo y verificación teórica FEM de las piezas sometidas a cargas en dicho dispositivo",
+
+        proy_inbelt_title:"VERIFICACION TEORICA ESCUDO MUELLE",
+        proy_inbelt_subtitle:"En este proyecto se verificó resistencia rigidez y estabilidad de una pieza fabricada de chapa IRAM IAS 215-6 F36 que tiene como fin absober el impacto de embarcaciones durante la maniobra de atraque. Se incluyó la verificación de cada cordón de soldadura. A tal fin se utilizaron herramientas de mecánica computacional basadas en la Teoría de la Elasticidad Lineal resuelta por el método de los elementos finitos.",
+
+        proy_gira_title:"GIRADOR MULTIPLES DE ESCAPE",
+        proy_gira_subtitle:"Ingeniería de detalle de un sujetador y girador de múltiples de escape de motores diesel para una estación de medición y ensamblado de accesorios en una fábrica de motores. El proyecto incluyó el modelado 3D completo, la confección de los pliegos constructivos y la asistencia en el ensamblaje",
+
+        proy_lg_title:"EQUIPO AUXILIAR DE IZAJE",
+        proy_lg_subtitle:"Diseño y fabricación de equipo auxiliar de izaje para montaje y desmontaje de medidores de gas.",
+
+        proy_saca_title: "HERRAMENTAL ACCIONAMIENTO SACABOCADOS" ,
+        proy_saca_subtitle:"Diseño y fabricación de herramental para accionamiento sacabocados en derivaciones T para líneas de gas. Cuenta con sistema de liberación rápida para intercambio de herramental.",
+
         destacados:"Nuestros proyectos destacados",
         slider1tit:"Rotador de block de motores",
         slider2tit:"Verificación teórica escudo muelle",
@@ -48,13 +63,26 @@ fr: {
      main_manip_title:"Manutention",
      main_manip_subtitle:"Conception et calcul d’éléments de levage et manutention pour tous les types d’industries et de besoins.",
 
-    main_calc_title:"Notes de Calcul",
+    main_calc_title:"Notes de Calcul",   
     main_calc_subtitle:"Réalisation de mémoires de calcul pour des modèles physiques de situations réelles réelles",
 
     main_simu_title:"simulations",
     main_simu_subtitle:"Simulations numériques de modèles physiques de statique, de dynamique, de transfert de chaleur, des vibrations, de la mécanique des fluides ainsi que de fatigue",
 
+    proy_rotweb_title:"ROTATEUR DE BLOC MOTEUR",
+    proy_rotweb_subtitle:"Dans ce projet, un rotateur de bloc moteur a été conçu pour les tâches de contrôle entre les processus d'usinage. Il comprenait des calculs dynamiques pour sélectionner la commande, des calculs pour le dimensionnement et la sélection du système pneumatique pour le mouvement et le serrage du châssis, et la vérification théorique FEM des pièces soumises à des charges dans ledit dispositif.",
+  
+    proy_inbelt_title:"BOUCLIER DE RESSORT DE VÉRIFICATION THÉORIQUE",
+    proy_inbelt_subtitle:"Dans ce projet, la résistance, la rigidité et la stabilité d'une pièce en tôle IRAM IAS 215-6 F36 ont été vérifiées, dont le but est d'absorber l'impact des bateaux lors de la manœuvre d'accostage. La vérification de chaque cordon de soudure a été incluse. À cette fin, des outils de mécanique computationnelle basés sur la théorie de l'élasticité linéaire résolue par la méthode des éléments finis ont été utilisés",
 
+    proy_gira_title:"ESSORAGE D’ÉCHAPPEMENT MULTIPLE",
+    proy_gira_subtitle:"Ingénierie détaillée d’une fixation et d’un centrifugeur d’échappement de moteur diesel pour une station de mesure et un ensemble d’accessoires dans une usine de moteurs. Le projet comprenait la modélisation 3D complète, la préparation des documents de construction et l’assistance au montage.",
+
+    proy_lg_title:"",
+    proy_lg_subtitle:"",
+
+
+    
     destacados:"le trabajó un montón",
     slider1tit:"Rotate le blocké de motoré",
     slider2tit:"la verifique  soi",
@@ -85,6 +113,21 @@ en: {
 
     main_simu_title:"Simulations",
     main_simu_subtitle:"Computational simulations of physical models of statics, dynamics, heat transfer, vibration, fluid mechanics, and fatigue",
+
+    proy_rotweb_title:"ENGINE BLOCK ROTATOR",
+    proy_rotweb_subtitle:"In this project, a motor block rotator was designed for control tasks between machining processes. It included dynamic calculations to select the command, calculations for sizing and selection of the pneumatic system for frame movement and block clamping, and FEM theoretical verification of the parts subjected to loads in said device.",
+
+    proy_inbelt_title:"THEORETICAL VERIFICATION for PORT SHIELD",
+    proy_inbelt_subtitle:"In this project, resistance, rigidity and stability of a piece made of IRAM IAS 215-6 F36 sheet was verified, which is intended to absorb the impact of vessels during the docking maneuver. Verification of each weld bead was included. For this, computational mechanics tools based on the Linear Elasticity Theory solved by the finite element method were used.",
+
+    proy_gira_title:"MULTIPLE EXHAUST SPINNER",
+    proy_gira_subtitle:"Detailed engineering of a diesel engine exhaust manifold fastener and spinner for a measuring station and accessory assembly in an engine factory. The project included the complete 3D modeling, the preparation of the construction documents and the assistance in the assembly.",
+
+    proy_lg_title:"AUXILIARY LIFTING DEVICE",
+    proy_lg_subtitle:"Design and manufacturing of auxiliary lifting device for gas meter mounting and dismounting.",
+
+    proy_saca_title:"CUSTOM TOOL FOR T's SOCKET PUNCH",
+    proy_caca_subtitle:"Design and manufacturing of custom tool for manual driving of "T"´s socket punches in gas lines. the tool has a quick release system.",
 
 
     destacados:"Our highlighted projects",
