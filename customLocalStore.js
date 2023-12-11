4be2d0c73153bdf5d0a31f6f26fec49de68fc197
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-12-11 03:32:27 -0300

    language and gallery changes

--- customLocalStore.js
+++ customLocalStore.js
@@ -1,10 +1,7 @@
 const translations = {
     es: {
-        /*Hero slide main ES*/ 
         main_title: "Excelencia en Servicios de Ingeniería",
         main_subtitle:"Trabajando con equipos de profesionales altamente calificados desde nuestra sede en Toulouse, con una vasta experiencia en diferentes rubros de la industria y utilizando las mejores herramientas del mercado, acercamos soluciones eficaces a la más variada gama de necesidades",
-        
-        /*expertise siderurgica ES*/
         main_sider_title:"Para la Industria Siderúrgica",
 
         main_sider_sub1:"Laminación en caliente ",
@@ -14,62 +11,37 @@ const translations = {
         main_sider_sub5:"Colada contínua",
         main_sider_sub6:"Parques de tratamiento de chatarra",
 
-        /*expertise Alimenticia ES*/
         main_alim_title:"Para la Industria Alimenticia",
         main_alim_subtitle:"Equipamiento y estructuras para plantas de procesado de especias, chocolate, té, etc",
         
-        /*expertise Manipulacion ES*/
-
         main_manip_title:"En Diseño de Elementos de Manipulación",
         main_manip_subtitle:"Diseño y cálculo de elementos de izaje y manipulación para todo tipo de industrias y necesidades.",
-        
-        /*expertise Calculo ES*/
+
         main_calc_title:"En Memorias de Cálculo",
         main_calc_subtitle:"Confección de memorias de cálculo para modelos físicos de situaciones reales",
-        
-        /*espertise simulaciones ES*/
         main_simu_title:"Simulaciones",
         main_simu_subtitle:"Simulaciones computacionales de modelos físicos de estática, dinámica, transferencia de calor, vibraciones, mecánica de fluidos y fatiga.",
+        proyectos_title:"Nuestros proyectos destacados",
 
         proy_rotweb_title:"ROTADOR DE BLOCKS DE MOTORES",
-        proy_rotweb_subtitle:"En este proyecto se diseñó un rotador de blocks de motor para tareas de control entre procesos de mecanizado. Incluyó cálculos dinámicos para seleccionar mando, cálculos para dimensionado y selección de sistema neumático para movimiento de marco y clampeo y verificación teórica FEM de las piezas sometidas a cargas en dicho dispositivo",
-
-        proy_inbelt_title:"VERIFICACION TEORICA ESCUDO MUELLE",
-        proy_inbelt_subtitle:"En este proyecto se verificó resistencia rigidez y estabilidad de una pieza fabricada de chapa IRAM IAS 215-6 F36 que tiene como fin absober el impacto de embarcaciones durante la maniobra de atraque. Se incluyó la verificación de cada cordón de soldadura. A tal fin se utilizaron herramientas de mecánica computacional basadas en la Teoría de la Elasticidad Lineal resuelta por el método de los elementos finitos.",
-
-        proy_gira_title:"GIRADOR MULTIPLES DE ESCAPE",
-        proy_gira_subtitle:"Ingeniería de detalle de un sujetador y girador de múltiples de escape de motores diesel para una estación de medición y ensamblado de accesorios en una fábrica de motores. El proyecto incluyó el modelado 3D completo, la confección de los pliegos constructivos y la asistencia en el ensamblaje",
+        proy_rotweb_breve:"Un rotador de blocks de motor para tareas de control entre procesos de mecanizado",
+        proy_rotweb_subtitle:"En este proyecto se diseñó un rotador de blocks de motor para tareas de control entre procesos de mecanizado. Incluyó cálculos dinámicos para seleccionar mando, cálculos para dimensionado y selección de sistema neumático para movimiento de marco y clampeo y verificación teórica FEM de las piezas sometidas a cargas en dicho dispositivo.",
 
         proy_lg_title:"EQUIPO AUXILIAR DE IZAJE",
+        proy_lg_breve:"Diseño y fabricación de equipo auxiliar de izaje para montaje y desmontaje de medidores de gas",
         proy_lg_subtitle:"Diseño y fabricación de equipo auxiliar de izaje para montaje y desmontaje de medidores de gas.",
-
-        proy_saca_title: "HERRAMENTAL ACCIONAMIENTO SACABOCADOS" ,
-        proy_saca_subtitle:"Diseño y fabricación de herramental para accionamiento sacabocados en derivaciones T para líneas de gas. Cuenta con sistema de liberación rápida para intercambio de herramental.",
-
-        compromiso_title:"",
-
-        compro_slide1_title:"",
-        compro_slide1_subtitle:"",
-        compro_slide1_1:"",
-        compro_slide1_2:"",
-        compro_slide1_3:"",
-        compro_slide1_4:"",
-        compro_slide2_title:"",
-        compro_slide2_subtitle:"",
-        compro_slide2_1:"",
-        compro_slide2_2:"",
-        compro_slide2_3:"",
-        compro_slide2_4:"",
-        compro_slide3_title:"",
-        compro_slide3_subtitle:"",
-        compro_slide_1:"",
         
+        proy_gira_title:"GIRADOR MULTIPLES DE ESCAPE",
+        proy_gira_breve:"Sujetador y girador de múltiples de escape de motores diesel para una estación de medición y ensamblado de accesorios en una fábrica de motores",
+        proy_gira_subtitle:"Ingeniería de detalle de un sujetador y girador de múltiples de escape de motores diesel para una estación de medición y ensamblado de accesorios en una fábrica de motores. El proyecto incluyó el modelado 3D completo, la confección de los pliegos constructivos y la asistencia en el ensamblaje",
+        
+        proy_dev_title:"DEVANADOR ALAMBRE COBRE Y EXTRUSORA DE PLÁSTICO",
+        proy_dev_breve:"Máquina devanadora de alambres y una extrusora de plástico para una línea contínua de fabricación de cables",
+        proy_dev_subtitle:"Ingeniería de detalle de una máquina devanadora de alambres y una extrusora de plástico a partir de un diseño conceptual propuesto por el cliente para una línea contínua de fabricación de cables. El proyecto incluyó el modelado 3D completo de ambas máquinas y la confección de los pliegos constructivos.",
 
-
-
-
+        
+},
 fr: {
-    /*Hero slide main FR*/
     main_title: 'L’Excellence des Services  D’Ingeniénierie',
      main_subtitle: 'En travaillant avec des équipes de professionnels hautement qualifiés de notre siège à Toulouse, avec une vaste expérience dans différents domaines de l’industrie et en utilisant les meilleurs outils du marché, nous apportons des solutions efficaces aux besoins les plus variés.',
      main_sider_title: "Sidérurgie",
@@ -80,47 +52,37 @@ fr: {
      main_sider_sub5:"Coulée continue",
      main_sider_sub6:"Traitement de la ferraille",
 
-     /*expertise alimentcia FR */
-
      main_alim_title:"Alimentarire",
      main_alim_subtitle:"Équipements et structures pour les industries des épices, du chocolat, du thé, etc",
-     /*expertise manutention FR */
+
      main_manip_title:"Manutention",
      main_manip_subtitle:"Conception et calcul d’éléments de levage et manutention pour tous les types d’industries et de besoins.",
 
     main_calc_title:"Notes de Calcul",
     main_calc_subtitle:"Réalisation de mémoires de calcul pour des modèles physiques de situations réelles réelles",
-    /*expertise calculo FR*/ 
     main_simu_title:"simulations",
     main_simu_subtitle:"Simulations numériques de modèles physiques de statique, de dynamique, de transfert de chaleur, des vibrations, de la mécanique des fluides ainsi que de fatigue",
-
+   
+    proyectos_title:"Nos Projets Phares",
+    
     proy_rotweb_title:"ROTATEUR DE BLOC MOTEUR",
+    proy_rotweb_breve:"Un rotateur de bloc moteur pour les tâches de contrôle entre les processus d'usinage",
     proy_rotweb_subtitle:"Dans ce projet, un rotateur de bloc moteur a été conçu pour les tâches de contrôle entre les processus d'usinage. Il comprenait des calculs dynamiques pour sélectionner la commande, des calculs pour le dimensionnement et la sélection du système pneumatique pour le mouvement et le serrage du châssis, et la vérification théorique FEM des pièces soumises à des charges dans ledit dispositif.",
-  
-    proy_inbelt_title:"BOUCLIER DE RESSORT DE VÉRIFICATION THÉORIQUE",
-    proy_inbelt_subtitle:"Dans ce projet, la résistance, la rigidité et la stabilité d'une pièce en tôle IRAM IAS 215-6 F36 ont été vérifiées, dont le but est d'absorber l'impact des bateaux lors de la manœuvre d'accostage. La vérification de chaque cordon de soudure a été incluse. À cette fin, des outils de mécanique computationnelle basés sur la théorie de l'élasticité linéaire résolue par la méthode des éléments finis ont été utilisés",
 
+    proy_lg_title:"DISPOSITIF DE LEVAGE AUXILIAIRE",
+    proy_lg_breve:"Conception et fabrication d'équipements auxiliaires de levage pour le montage et le démontage de compteurs de gaz.",
+    proy_lg_subtitle:"Conception et fabrication d'équipements auxiliaires de levage pour le montage et le démontage de compteurs de gaz.",
+    
     proy_gira_title:"ESSORAGE D’ÉCHAPPEMENT MULTIPLE",
+    proy_gira_breve:"Ingénierie détaillée d’une fixation et d’un centrifugeur d’échappement de moteur diesel pour une station de mesure et un ensemble d’accessoires dans une usine de moteurs.",
     proy_gira_subtitle:"Ingénierie détaillée d’une fixation et d’un centrifugeur d’échappement de moteur diesel pour une station de mesure et un ensemble d’accessoires dans une usine de moteurs. Le projet comprenait la modélisation 3D complète, la préparation des documents de construction et l’assistance au montage.",
 
-    proy_lg_title:"",
-    proy_lg_subtitle:"",
+    proy_dev_title:"ENROULEUR DE FIL DE CUIVRE ET EXTRUDEUR DE PLASTIQUE",
+    proy_dev_breve:"Bobineuse de fil et extrudeuse de plastique pour une ligne de fabrication de câbles en continu",
+    proy_dev_subtitle:"Ingénierie de détail d'une machine de bobinage de fil et d'une extrudeuse de plastique basée sur une conception proposée par le client pour une ligne de fabrication de câbles continus. Le projet comprenait la modélisation 3D complète des deux machines et la préparation des documents de construction.",
 
-    proy_saca_title:"",
-    proy_saca_subtitle:"",
-
-
-
-    destacados:"le trabajó un montón",
-    slider1tit:"Rotate le blocké de motoré",
-    slider2tit:"la verifique  soi",
-    slider3tit:"frances",
-    slider4tit:"Giradre le escapé ",
-    slider5tit:"francia c de plástico",
-    slider6tit:"Gfransois le pifie",
-},
+    },
 en: {
-    /*Hero slide main ENG*/
     main_title: 'Excellence in Engineering Services',
     main_subtitle: 'Working with teams of highly qualified professionals from our headquarters in Toulouse, with vast experience in different areas of the industry and using the best tools on the market, we bring effective solutions to the most varied range of needs.',
     main_sider_title: "For Steelmaking",
@@ -130,35 +92,35 @@ en: {
     main_sider_sub4:"Direct Reduction",
     main_sider_sub5:"Continuous casting",
     main_sider_sub6:"Scrap Metal Parks",
-    
-    /*expertise alimenticia EN*/
 
     main_alim_title:"For Food Industry",
     main_alim_subtitle:"Equipment and structures for spices processing plants, chocolate, tea, etc",
-    /*expertise manipulacion EN*/ 
+
     main_manip_title:"In Handling Element Design",
     main_manip_subtitle:"Design and calculation of lifting and handling elements for all types of industries and needs",
-    /*expertise calculo EN*/
+
     main_calc_title:"In Calculation Reports",
     main_calc_subtitle:"Preparation of calculation reports for physical models of real phenomena",
-    /*expertise simulacion EN*/
     main_simu_title:"Simulations",
     main_simu_subtitle:"Computational simulations of physical models of statics, dynamics, heat transfer, vibration, fluid mechanics, and fatigue",
+    
+    proyectos_title:"Our Featured Projects",
 
     proy_rotweb_title:"ENGINE BLOCK ROTATOR",
-    proy_rotweb_subtitle:"In this project, a motor block rotator was designed for control tasks between machining processes. It included dynamic calculations to select the command, calculations for sizing and selection of the pneumatic system for frame movement and block clamping, and FEM theoretical verification of the parts subjected to loads in said device.",
-
-    proy_inbelt_title:"THEORETICAL VERIFICATION for PORT SHIELD",
-    proy_inbelt_subtitle:"In this project, resistance, rigidity and stability of a piece made of IRAM IAS 215-6 F36 sheet was verified, which is intended to absorb the impact of vessels during the docking maneuver. Verification of each weld bead was included. For this, computational mechanics tools based on the Linear Elasticity Theory solved by the finite element method were used.",
-
-    proy_gira_title:"MULTIPLE EXHAUST SPINNER",
-    proy_gira_subtitle:"Detailed engineering of a diesel engine exhaust manifold fastener and spinner for a measuring station and accessory assembly in an engine factory. The project included the complete 3D modeling, the preparation of the construction documents and the assistance in the assembly.",
-
+    proy_rotweb_breve:"A motor block rotator for control tasks between machining processes",
+   
     proy_lg_title:"AUXILIARY LIFTING DEVICE",
+    proy_lg_breve:"Design and manufacturing of auxiliary lifting device for gas meter mounting and dismounting.",
     proy_lg_subtitle:"Design and manufacturing of auxiliary lifting device for gas meter mounting and dismounting.",
 
-    proy_saca_title:"CUSTOM TOOL FOR T's SOCKET PUNCH",
-    proy_caca_subtitle:"Design and manufacturing of custom tool for manual driving of "T"´s socket punches in gas lines. the tool has a quick release system.",
+    proy_gira_title:"MULTIPLE EXHAUST SPINNER",
+    proy_gira_breve:"Detailed engineering of a diesel engine exhaust manifold fastener and spinner for a measuring station and accessory assembly in an engine factory.",
+    proy_gira_subtitle:"Detailed engineering of a diesel engine exhaust manifold fastener and spinner for a measuring station and accessory assembly in an engine factory. The project included the complete 3D modeling, the preparation of the construction documents and the assistance in the assembly.",
+
+    proy_dev_title:"COPPER WIRE WINDER AND PLASTIC EXTRUDER",
+    proy_dev_breve:"Wire winding machine and a plastic extruder for a continuous cable manufacturing line",
+    proy_dev_subtitle:"Detail engineering of a wire winding machine and a plastic extruder based on a conceptual design proposed by the client for a continuous cable manufacturing line. The project included the complete 3D modeling of both machines and the preparation of the construction documents.",
+
 
 
 },
---
+++ feed.js
@@ -0,0 +1,139 @@
+const translations = {
+    es: {
+      
+      
+        proy_inbelt_title:"VERIFICACION TEORICA ESCUDO MUELLE",
+        proy_inbelt_breve:"Se verificó resistencia rigidez y estabilidad de una pieza fabricada de chapa IRAM IAS 215-6 F36 que tiene como fin absober el impacto de embarcaciones durante la maniobra de atraque",
+        proy_inbelt_subtitle:"En este proyecto se verificó resistencia rigidez y estabilidad de una pieza fabricada de chapa IRAM IAS 215-6 F36 que tiene como fin absober el impacto de embarcaciones durante la maniobra de atraque. Se incluyó la verificación de cada cordón de soldadura. A tal fin se utilizaron herramientas de mecánica computacional basadas en la Teoría de la Elasticidad Lineal resuelta por el método de los elementos finitos.",
+
+        proy_gira_title:"GIRADOR MULTIPLES DE ESCAPE",
+        proy_gira_breve:"Sujetador y girador de múltiples de escape de motores diesel para una estación de medición y ensamblado de accesorios en una fábrica de motores",
+        proy_gira_subtitle:"Ingeniería de detalle de un sujetador y girador de múltiples de escape de motores diesel para una estación de medición y ensamblado de accesorios en una fábrica de motores. El proyecto incluyó el modelado 3D completo, la confección de los pliegos constructivos y la asistencia en el ensamblaje",
+
+        proy_lg_title:"EQUIPO AUXILIAR DE IZAJE",
+        proy_lg_breve:"Diseño y fabricación de equipo auxiliar de izaje para montaje y desmontaje de medidores de gas",
+        proy_lg_subtitle:"Diseño y fabricación de equipo auxiliar de izaje para montaje y desmontaje de medidores de gas.",
+
+        proy_saca_title: "HERRAMENTAL ACCIONAMIENTO SACABOCADOS" ,
+        proy_saca_breve: "Diseño y fabricación de herramental para accionamiento sacabocados en derivaciones T para líneas de gas",
+        proy_saca_subtitle:"Diseño y fabricación de herramental para accionamiento sacabocados en derivaciones T para líneas de gas. Cuenta con sistema de liberación rápida para intercambio de herramental.",
+
+        proy_dev_title:"DEVANADOR ALAMBRE COBRE Y EXTRUSORA DE PLÁSTICO",
+        proy_dev_breve:"Máquina devanadora de alambres y una extrusora de plástico para una línea contínua de fabricación de cables",
+        proy_dev_subtitle:"Ingeniería de detalle de una máquina devanadora de alambres y una extrusora de plástico a partir de un diseño conceptual propuesto por el cliente para una línea contínua de fabricación de cables. El proyecto incluyó el modelado 3D completo de ambas máquinas y la confección de los pliegos constructivos.",
+
+
+       /* compromiso_title:"",
+
+        compro_slide1_title:"",
+        compro_slide1_subtitle:"",
+        compro_slide1_1:"",
+        compro_slide1_2:"",
+        compro_slide1_3:"",
+        compro_slide1_4:"",
+        compro_slide2_title:"",
+        compro_slide2_subtitle:"",
+        compro_slide2_1:"",
+        compro_slide2_2:"",
+        compro_slide2_3:"",
+        compro_slide2_4:"",
+        compro_slide3_title:"",
+        compro_slide3_subtitle:"",
+        compro_slide_1:"",*/
+        
+
+
+
+
+fr: {
+    
+    proy_rotweb_title:"ROTATEUR DE BLOC MOTEUR",
+    proy_rotweb_subtitle:"Dans ce projet, un rotateur de bloc moteur a été conçu pour les tâches de contrôle entre les processus d'usinage. Il comprenait des calculs dynamiques pour sélectionner la commande, des calculs pour le dimensionnement et la sélection du système pneumatique pour le mouvement et le serrage du châssis, et la vérification théorique FEM des pièces soumises à des charges dans ledit dispositif.",
+  
+    proy_inbelt_title:"BOUCLIER DE RESSORT DE VÉRIFICATION THÉORIQUE",
+    proy_inbelt_subtitle:"Dans ce projet, la résistance, la rigidité et la stabilité d'une pièce en tôle IRAM IAS 215-6 F36 ont été vérifiées, dont le but est d'absorber l'impact des bateaux lors de la manœuvre d'accostage. La vérification de chaque cordon de soudure a été incluse. À cette fin, des outils de mécanique computationnelle basés sur la théorie de l'élasticité linéaire résolue par la méthode des éléments finis ont été utilisés",
+
+    proy_gira_title:"ESSORAGE D’ÉCHAPPEMENT MULTIPLE",
+    proy_gira_subtitle:"Ingénierie détaillée d’une fixation et d’un centrifugeur d’échappement de moteur diesel pour une station de mesure et un ensemble d’accessoires dans une usine de moteurs. Le projet comprenait la modélisation 3D complète, la préparation des documents de construction et l’assistance au montage.",
+
+    proy_lg_title:"DISPOSITIF DE LEVAGE AUXILIAIRE",
+    proy_lg_subtitle:"Conception et fabrication d'équipements auxiliaires de levage pour le montage et le démontage de compteurs de gaz.",
+
+    proy_saca_title:"Outillage d'entraînement du poinçon",
+    proy_saca_subtitle:"Conception et fabrication d'outillages pour enfoncer des poinçons dans les branches en "T" des conduites de gaz. Il dispose d'un système de libération rapide pour l'échange d'outils.",
+
+    proy_dev_title:"ENROULEUR DE FIL DE CUIVRE ET EXTRUDEUR DE PLASTIQUE",
+    proy_dev_breve:"Bobineuse de fil et extrudeuse de plastique pour une ligne de fabrication de câbles en continu",
+    proy_dev_subtitle:"Ingénierie de détail d'une machine de bobinage de fil et d'une extrudeuse de plastique basée sur une conception proposée par le client pour une ligne de fabrication de câbles continus. Le projet comprenait la modélisation 3D complète des deux machines et la préparation des documents de construction.",
+},
+en: {
+    
+
+    
+   
+    
+    /*proyectos section*/
+    proyectos_title:"Our Featured Projects",
+
+    proy_rotweb_title:"ENGINE BLOCK ROTATOR",
+    proy_rotweb_subtitle:"In this project, a motor block rotator was designed for control tasks between machining processes. It included dynamic calculations to select the command, calculations for sizing and selection of the pneumatic system for frame movement and block clamping, and FEM theoretical verification of the parts subjected to loads in said device.",
+
+    proy_inbelt_title:"THEORETICAL VERIFICATION for PORT SHIELD",
+    proy_inbelt_subtitle:"In this project, resistance, rigidity and stability of a piece made of IRAM IAS 215-6 F36 sheet was verified, which is intended to absorb the impact of vessels during the docking maneuver. Verification of each weld bead was included. For this, computational mechanics tools based on the Linear Elasticity Theory solved by the finite element method were used.",
+
+    proy_gira_title:"MULTIPLE EXHAUST SPINNER",
+    proy_gira_subtitle:"Detailed engineering of a diesel engine exhaust manifold fastener and spinner for a measuring station and accessory assembly in an engine factory. The project included the complete 3D modeling, the preparation of the construction documents and the assistance in the assembly.",
+
+    proy_lg_title:"AUXILIARY LIFTING DEVICE",
+    proy_lg_subtitle:"Design and manufacturing of auxiliary lifting device for gas meter mounting and dismounting.",
+
+    proy_saca_title:"CUSTOM TOOL FOR T's SOCKET PUNCH",
+    proy_caca_subtitle:"Design and manufacturing of custom tool for manual driving of "T"´s socket punches in gas lines. the tool has a quick release system.",
+
+    proy_dev_title:"COPPER WIRE WINDER AND PLASTIC EXTRUDER",
+    proy_dev_breve:"Wire winding machine and a plastic extruder for a continuous cable manufacturing line",
+    proy_dev_subtitle:"Detail engineering of a wire winding machine and a plastic extruder based on a conceptual design proposed by the client for a continuous cable manufacturing line. The project included the complete 3D modeling of both machines and the preparation of the construction documents.",
+
+    
+
+
+
+},
+
+};
+
+
+
+function changeLanguage(language) {
+    console.log(language);
+    const elements = document.querySelectorAll('[data-translation-id]');
+    console.log(elements);
+
+    elements.forEach(element => {
+        const translationId = element.getAttribute('data-translation-id');
+        element.textContent = translations[language][translationId];
+    });
+
+    // Store the selected language in localStorage
+    localStorage.setItem('selectedLanguage', language);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Retrieve selected language from localStorage on page load
+    const selectedLanguage = localStorage.getItem('selectedLanguage');
+
+    if (selectedLanguage) {
+        
+        changeLanguage('es');
+    }
+});
+
+
+
+
+
+
+
+
+
+
