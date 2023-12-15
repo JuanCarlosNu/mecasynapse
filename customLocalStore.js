4f91d32b2539a2a953192b3f10b1c39627224d34
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-12-15 06:55:46 -0300

    toda la noche le meti

--- customLocalStore.js
+++ customLocalStore.js
@@ -1,5 +1,12 @@
 const translations = {
     es: {
+        menu_inicio:"Inicio",
+        menu_acerca:"Acerca",
+        menu_proyectos:"Proyectos",
+        menu_contacto:"Contacto",
+
+        ver:"VER PROYECTO",
+
         main_title: "Excelencia en Servicios de Ingeniería",
         main_subtitle:"Trabajando con equipos de profesionales altamente calificados desde nuestra sede en Toulouse, con una vasta experiencia en diferentes rubros de la industria y utilizando las mejores herramientas del mercado, acercamos soluciones eficaces a la más variada gama de necesidades",
         main_sider_title:"Para la Industria Siderúrgica",
@@ -44,8 +51,23 @@ const translations = {
         proy_mue_breve:"Se verificó resistencia rigidez y estabilidad de una pieza fabricada de chapa IRAM IAS 215-6 F36 que tiene como fin absober el impacto de embarcaciones durante la maniobra de atraque",
         proy_mue_subtitle:"En este proyecto se verificó resistencia rigidez y estabilidad de una pieza fabricada de chapa IRAM IAS 215-6 F36 que tiene como fin absober el impacto de embarcaciones durante la maniobra de atraque. Se incluyó la verificación de cada cordón de soldadura. A tal fin se utilizaron herramientas de mecánica computacional basadas en la Teoría de la Elasticidad Lineal resuelta por el método de los elementos finitos.",
 
+        comp_title: "Nuestro compromiso",
+        comp_title1:"¿Qué servicios ofrecemos?",
+      comp_subtitle:"Nuestro compromiso es ofrecer un servicio de calidad para pymes y talleres mecánicos altamente calificados",
+           comp_1_1:"Diseño y análisis de máquinas y dispositivos con herramientas computacionales 3D.",
+           comp_1_2:"Confección de ingeniería de detalle para manufactura.",
+           comp_1_3:"Confección de planos mecánicos de fabricación.",
+           comp_1_4:"Cálculos y confección de memorias de cálculo de fenómenos de Estática, Dinámica, Calor, Vibraciones, Fatiga y Pandeo.",    
 },
-fr: {
+fr: {   
+        menu_inicio:"Début",
+        menu_acerca:"à propos",
+        menu_proyectos:"Projets",
+        menu_contacto:"Contact",
+
+        ver: "VOIR LE PROJET",
+
+
     main_title: 'L’Excellence des Services  D’Ingeniénierie',
      main_subtitle: 'En travaillant avec des équipes de professionnels hautement qualifiés de notre siège à Toulouse, avec une vaste expérience dans différents domaines de l’industrie et en utilisant les meilleurs outils du marché, nous apportons des solutions efficaces aux besoins les plus variés.',
      main_sider_title: "Sidérurgie",
@@ -90,8 +112,26 @@ fr: {
     proy_mue_breve:"On a vérifié la résistance, la rigidité et la stabilité d'une pièce en tôle IRAM IAS 215-6 F36 dont le but est d'absorber l'impact des navires lors de la manœuvre d'accostage.",
     proy_mue_subtitle:"Dans ce projet, la résistance, la rigidité et la stabilité d'une pièce en tôle IRAM IAS 215-6 F36 ont été vérifiées, dont le but est d'absorber l'impact des bateaux lors de la manœuvre d'accostage. La vérification de chaque cordon de soudure a été incluse. À cette fin, des outils de mécanique computationnelle basés sur la théorie de l'élasticité linéaire résolue par la méthode des éléments finis ont été utilisés.",
 
+       comp_title: "Nos Prestations",  
+        comp_title1:" quels services nous proposons",
+        comp_subtitle:"Notre engagement et fournir un service de BE de qualité pour les PME et les ateliers mécaniqueshautement qualifiés",
+           comp_1_1:"Conception et analyse de machines",
+           comp_1_2:"Réalisation de l’ingénierie détaillée pour la fabrication.",
+           comp_1_3:"Réalisation des plans de fabrication.",
+           comp_1_4:"Réalisation de notes de calcul.",
+
+
+
     },
 en: {
+    menu_inicio:"Home",
+    menu_acerca:"About",
+    menu_proyectos:"Projets",
+    menu_contacto:"Contact",
+
+    ver: "see project",
+
+
     main_title: 'Excellence in Engineering Services',
     main_subtitle: 'Working with teams of highly qualified professionals from our headquarters in Toulouse, with vast experience in different areas of the industry and using the best tools on the market, we bring effective solutions to the most varied range of needs.',
     main_sider_title: "For Steelmaking",
@@ -137,6 +177,14 @@ en: {
     proy_mue_breve:"The resistance, rigidity and stability of a piece made of IRAM IAS 215-6 F36 sheet metal was verified, the purpose of which is to absorb the impact of vessels during the docking maneuver.",
     proy_mue_subtitle:"In this project, resistance, rigidity and stability of a piece made of IRAM IAS 215-6 F36 sheet was verified, which is intended to absorb the impact of vessels during the docking maneuver. Verification of each weld bead was included. For this, computational mechanics tools based on the Linear Elasticity Theory solved by the finite element method were used.",
 
+    comp_title: "NOS PRESTATIONS",  
+        comp_title1:"Notre engagement et fournir un service de BE de qualité pour les PME et les ateliers mécaniqueshautement qualifiés",
+      comp_subtitle:"Nuestro compromiso es ofrecer un servicio de calidad para pymes y talleres mecánicos altamente calificados",
+           comp_1_1:"Conception et analyse de machines",
+           comp_1_2:"Réalisation de l’ingénierie détaillée pour la fabrication.",
+           comp_1_3:"Réalisation des plans de fabrication.",
+           comp_1_4:"Réalisation de notes de calcul.",
+           
 
 
 },
